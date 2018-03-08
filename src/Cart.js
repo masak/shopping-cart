3f5373ef013ba608da692ae2222e0f6323c01c16
Carl Masak <cmasak@gmail.com>
2018-03-08 09:36:16 +0100

    add new Cart class, and a test
    Remove the old Greeting class.

---
+++ src/Cart.js
@@ -0,0 +1,11 @@
+class Cart {
+
+  constructor() {
+  }
+
+  add() {
+  }
+
+}
+
+export default Cart;
---
+++ test/CartSpec.js
@@ -0,0 +1,12 @@
+import { expect } from 'chai';
+import Cart from '../src/Cart';
+
+describe('Cart', () => {
+
+  it('can add things to a shopping cart', () => {
+    const cart = new Cart();
+    const product = {};
+    cart.add(product);
+  });
+
+});
